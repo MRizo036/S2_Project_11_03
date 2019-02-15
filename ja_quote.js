2d4452854420eb19b148e22114de56e79934a8df
Unknown <mrizo036@west-mec.org>
2019-02-14 17:14:29 -0700

    Final Commit
    All HTML and JavaScript completed. Comments added. Ready for Review.

--- ja_quote.js
+++ ja_quote.js
@@ -14,14 +14,19 @@
    quotation tag in the Web page.
 
 */
-/*  */
+/* The code below was done with the help of Mr.Buckler. This means that the code may not exactly be written in the manner in which  the textboom specifies but still serves the same purpose as it is supposed to. */
+
+/* The var below creates a variable named size which is set equal to the value of 10. */
 var size = 10;
+/* The line below creates a variable called randomQ which is set equal to the value of the function randomInt. Inside of randomInt function, the parameters of the functions are set to 0 and size. */
 var randomQ = randomInt(0, size);
-// alert("randomQ: " + randomQ);
+/* alert("randomQ: " + randomQ); 
+This was added in order to test the values that were being put out by the randomQ variable. */
+/* The line below states the vlaue of the element with the id of quote will be changed within the HTML document. Setting the getQuote(randomQ) chooses a random quote from the getQuote function which are numbered from 0 to 9 */
 document.getElementById("quote").innerHTML = getQuote(randomQ);
-
+/* The line below creates a function names randomInt. The functions is given parameters inside of the paranthesis which are lowest(equal to zero) and size(set in the first line and equal to 10). */
 function randomInt(lowest, size) {
-
+    /* The line bolew will generate an whole integer rounded down randomely mutiplied by the value of size. This value is then returned as the function. */
     return Math.floor(Math.random() * size);
 }
 
